0af740563ab71300d5fe83543087c2bf52e325af
Daichi-Takase <toisu.takase@gmail.com>
2021-10-25 18:24:17 +0900

    テストクラス修正、例外。チェック処理追加

--- src/main/java/com/mycompany/subject/service/JdbcMain.java
+++ src/main/java/com/mycompany/subject/service/JdbcMain.java
@@ -7,6 +7,7 @@ package com.mycompany.subject.service;
 import com.mycompany.subject.dao.MemberDao;
 import com.mycompany.subject.dto.MemberDto;
 import com.mycompany.subject.entity.MemberEntity;
+import static com.mycompany.subject.service.JdbcMain.insertMember;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -16,8 +17,10 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.format.ResolverStyle;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
+import org.apache.commons.lang.StringUtils;
 
 /**
  *
@@ -26,79 +29,78 @@ import java.util.regex.Pattern;
 public class JdbcMain {
 
     public static void main(String[] args) {
-        Connection conn = null;
 
         //接続文字列
         String url = "jdbc:postgresql://localhost:5432/postgres";
         String user = "postgres";
         String password = "postgres";
 
+        MemberDto memberDto = new MemberDto();
+        int record = insertMember(memberDto, url, user, password);
+
+    }
+
+    /* 会員登録実行 */
+    public static int insertMember(MemberDto mDto, String url, String user, String password) {
+
+        int r = 0;
+        boolean exeFlg = true;
+        Connection conn = null;
+
         try {
             //PostgreSQLへ接続
             conn = DriverManager.getConnection(url, user, password);
 
-            MemberDto memberDto = new MemberDto();
             MemberEntity memberEntity = new MemberEntity();
             MemberDao memberDao = new MemberDao(conn);
 
-            /* 画面入力項目セット（仮）*/
-            //名前
-            memberDto.setName("高瀬");
-            //性別
-            memberDto.setSex("男性");
-            //生年月日（年・月・日）
-            memberDto.setYear("1991");
-            memberDto.setMonth("10");
-            memberDto.setDay("05");
-            //郵便番号
-            memberDto.setPostal_code("530-0037");
-            //住所
-            memberDto.setAddress("大阪府大阪市北区");
-            //電話番号
-            memberDto.setTelephone_no("08012345678");
-            //クレジットカード番号
-            memberDto.setCredit_no("1234567890123456");
-
             /* DB更新項目セット */
             //名前
-            memberEntity.setName(memberDto.getName());
+            memberEntity.setName(mDto.getName());
             //性別
-            SexInfo si = SexInfo.getByCd(memberDto.getSex());
-            memberEntity.setSex(String.valueOf(si.getCd()));
+            SexInfo si = SexInfo.getByCd(mDto.getSex());
+            if (si != null) {
+                memberEntity.setSex(String.valueOf(si.getCd()));
+            }
+            exeFlg = false;
+            System.out.println("性別が不正です。");
+
             //生年月日
-            String ymd = memberDto.getYear() + "-" + memberDto.getMonth() + "-" + memberDto.getDay();
+            String ymd = mDto.getYear() + "-" + mDto.getMonth() + "-" + mDto.getDay();
             try {
                 LocalDate.parse(ymd, DateTimeFormatter.ofPattern("uuuu-MM-dd").withResolverStyle(ResolverStyle.STRICT));
             } catch (DateTimeParseException dtp) {
                 dtp.printStackTrace();
+                exeFlg = false;
+                System.out.println("生年月日が不正です。");
             }
             java.sql.Date date_of_birth = java.sql.Date.valueOf(ymd);
             memberEntity.setDate_of_birth(date_of_birth);
             //郵便番号
-            if (isZipCode(memberDto.getPostal_code())) {
-                memberEntity.setPostal_code(memberDto.getPostal_code());
+            if (isZipCode(mDto.getPostal_code())) {
+                memberEntity.setPostal_code(mDto.getPostal_code());
             } else {
-                memberEntity.setPostal_code("");
+                exeFlg = false;
+                System.out.println("郵便番号が不正です。");
             }
             //住所
-            memberEntity.setAddress(memberDto.getAddress());
+            memberEntity.setAddress(mDto.getAddress());
             //電話番号
-            memberEntity.setTelephone_no(memberDto.getTelephone_no());
+            memberEntity.setTelephone_no(mDto.getTelephone_no());
             //クレジットカード番号
-            memberEntity.setCredit_no(memberDto.getCredit_no());
+            if (creditCheckDigit(mDto.getCredit_no()) == Integer.parseInt(mDto.getCredit_no().substring(15, 16))) {
+            memberEntity.setCredit_no(mDto.getCredit_no());
+            } else {
+                exeFlg = false;
+                System.out.println("クレジットカード番号が不正です。");
+            }
             //登録日
             LocalDateTime localDateTime = LocalDateTime.now();
             java.sql.Timestamp creation_date_time = java.sql.Timestamp.valueOf(localDateTime);
             memberEntity.setCreation_date_time(creation_date_time);
 
-            /* 会員登録実行 */
-            //memberDao.setAll(memberEntity);
-            
-            /* 会員参照実行 */
-            List<MemberEntity> data = new ArrayList<MemberEntity>();
-            data = memberDao.getMember("3");
-            for (MemberEntity d : data) {
-                System.out.println(d.getName());
+            if (exeFlg) {
+                r = memberDao.setAll(memberEntity);
             }
 
         } catch (SQLException e) {
@@ -110,6 +112,39 @@ public class JdbcMain {
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
+            }
+        }
+        return r;
+    }
+
+    /* 会員参照実行 */
+    public static void selectMember(MemberDto mDto, String url, String user, String password) {
+
+        Connection conn = null;
+
+        try {
+
+            //PostgreSQLへ接続
+            conn = DriverManager.getConnection(url, user, password);
+
+            MemberEntity memberEntity = new MemberEntity();
+            MemberDao memberDao = new MemberDao(conn);
+            List<MemberEntity> data = new ArrayList<MemberEntity>();
+
+            data = memberDao.getMember(mDto.getId());
+            for (MemberEntity d : data) {
+                System.out.println(d.getName() + ", " + d.getSex() + ", " + d.getDate_of_birth());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+
             }
         }
     }
@@ -138,27 +173,24 @@ public class JdbcMain {
 
         //性別に一致するコードを返却
         public static SexInfo getByCd(String sex) {
-            for (SexInfo si : SexInfo.values()) {
-                if (si.getSex().equals(sex)) {
-                    return si;
-                }
-            }
-            return null;
-        }
 
-        //コードに一致する性別を返却
-        public static SexInfo getBySex(int cd) {
-            for (SexInfo si : SexInfo.values()) {
-                if (si.getCd() == cd) {
-                    return si;
-                }
-            }
-            return null;
+            return Arrays.stream(SexInfo.values())
+                    .filter(data -> data.getSex().equals(sex))
+                    .findFirst()
+                    .orElse(null);
         }
-        
+        //コードに一致する性別を返却
+
+        public static SexInfo getBySex(int cd) {
+
+            return Arrays.stream(SexInfo.values())
+                    .filter(data -> data.getCd() == (cd))
+                    .findFirst()
+                    .orElse(null);
+        }
     }
 
-    //郵便番号チェック
+//郵便番号チェック
     public static boolean isZipCode(String value) {
         boolean result = false;
         if (value != null) {
@@ -168,4 +200,50 @@ public class JdbcMain {
         return result;
     }
 
+    //クレジットカード（CheckDigit）
+    public static int creditCheckDigit(String creditNo) {
+
+        //引数チェック
+        if (creditNo == null) {
+            throw new IllegalArgumentException("引数にnullは指定不可");
+        }
+        if (creditNo.length() != 16) {
+            throw new IllegalArgumentException("引数は16桁必須");
+        }
+        if (!StringUtils.isNumeric(creditNo)) {
+            throw new IllegalArgumentException("引数は数値必須");
+        }
+
+        char[] creditNoArgs = creditNo.toCharArray();
+        String[] calcResultArgs = new String[15];
+        
+        for (int i = 0; i < creditNoArgs.length - 1; i++) {
+            // 1桁目（i = 0：1回目）の場合、係数×２
+            if (i % 2 == 0) {
+                calcResultArgs[14 - i] = String.valueOf(Character.getNumericValue(creditNoArgs[14 - i]) * 2);
+            } else {
+                calcResultArgs[14 - i] = String.valueOf(Character.getNumericValue(creditNoArgs[14 - i]) * 1);
+            }
+        }
+        
+        //2桁以上は分割し、加算
+        int calcTotal = 0;
+        for (String cra : calcResultArgs) {
+            if (cra.length() >= 2) {
+                calcTotal = calcTotal + Integer.parseInt(cra.substring(0, 1));
+                calcTotal = calcTotal + Integer.parseInt(cra.substring(1, 2));
+            } else {
+                calcTotal = calcTotal + Integer.parseInt(cra);
+            }
+        }
+
+        // 10で割った余り
+        int amari = calcTotal % 10;
+
+        // 10-(10で割ったあまり)がチェックディジット
+        int digit = 10 - amari;
+        
+        return digit;
+        
+    }
 }
--- src/test/java/com/mycompany/subject/service/JdbcMainTest.java
+++ src/test/java/com/mycompany/subject/service/JdbcMainTest.java
@@ -4,12 +4,13 @@
  */
 package com.mycompany.subject.service;
 
+import com.mycompany.subject.dto.MemberDto;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 import org.junit.Ignore;
-
 
 /**
  *
@@ -17,15 +18,90 @@ import org.junit.Ignore;
  */
 public class JdbcMainTest {
 
+    //接続文字列
+    String url = "jdbc:postgresql://localhost:5432/postgres";
+    String user = "postgres";
+    String password = "postgres";
+
     @Test
     public void test0() {
         fail("作成中");
     }
- 
+
+    @Test
+    public void testinsertMember1() {
+
+        /* 画面入力項目セット（仮）*/
+        MemberDto memberDto = new MemberDto();
+        //名前
+        memberDto.setName("高瀬");
+        //性別
+        memberDto.setSex("男性");
+        //生年月日（年・月・日）
+        memberDto.setYear("1991");
+        memberDto.setMonth("10");
+        memberDto.setDay("05");
+        //郵便番号
+        memberDto.setPostal_code("530-0037");
+        //住所
+        memberDto.setAddress("大阪府大阪市北区");
+        //電話番号
+        memberDto.setTelephone_no("08012345678");
+        //クレジットカード番号
+        memberDto.setCredit_no("1234567890123452");
+
+        int result = JdbcMain.insertMember(memberDto, url, user, password);
+        assertEquals(1, result);
+
+    }
+
+    @Test
+    public void testinsertMember2() {
+
+        /* 画面入力項目セット（仮）*/
+        MemberDto memberDto = new MemberDto();
+        //名前
+        memberDto.setName("高瀬");
+        //性別
+        memberDto.setSex("性別なし");
+        //生年月日（年・月・日）
+        memberDto.setYear("1991");
+        memberDto.setMonth("10");
+        memberDto.setDay("05");
+        //郵便番号
+        memberDto.setPostal_code("530-0037");
+        //住所
+        memberDto.setAddress("大阪府大阪市北区");
+        //電話番号
+        memberDto.setTelephone_no("08012345678");
+        //クレジットカード番号
+        memberDto.setCredit_no("1234567890123452");
+
+        int result = JdbcMain.insertMember(memberDto, url, user, password);
+        assertEquals(0, result);
+
+    }
+
+    @Test
+    public void testselectMember1() {
+
+        /* 画面入力項目セット（仮）*/
+        MemberDto memberDto = new MemberDto();
+        //ID
+        memberDto.setId("3");
+
+    }
+
     @Test
     public void testgetByCd1() {
-        JdbcMain.SexInfo si = JdbcMain.SexInfo.getByCd("男性");
-        assertThat(si.getCd(), is(1));
+        JdbcMain.SexInfo si = JdbcMain.SexInfo.getByCd("女性");
+        assertThat(si.getCd(), is(2));
+    }
+
+    @Test
+    public void testgetBySex1() {
+        JdbcMain.SexInfo si = JdbcMain.SexInfo.getBySex(1);
+        assertThat(si.getSex(), is("男性"));
     }
 
     @Test
@@ -38,6 +114,24 @@ public class JdbcMainTest {
     public void testisZipCode2() {
         boolean result = JdbcMain.isZipCode("〒530-0037");
         assertThat(result, is(false));
+    }
+
+    @Test
+    public void testisZipCode3() {
+        boolean result = JdbcMain.isZipCode(null);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void testcreditCheckDigit1() {
+        int result = JdbcMain.creditCheckDigit("1234567890123452");
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testcreditCheckDigit2() {
+        int result = JdbcMain.creditCheckDigit("1234567890123453");
+        assertEquals(3, result);
     }
 
 }
